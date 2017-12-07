f3f4f0d9b8cbdaabacf8d0b102668971e36dfc13
eugenevorld <eugene_xhuang@hotmail.com>
2017-12-07 15:10:25 +0800

    made ResourceToken generic

--- src/opendrop/utility/resources.py
+++ src/opendrop/utility/resources.py
@@ -1,6 +1,6 @@
 from abc import abstractmethod
 
-from typing import Type, Any, List, Set
+from typing import Any, Generic, List, Set, Type, TypeVar
 
 
 class IResource:
@@ -8,16 +8,19 @@ class IResource:
     def destroy(self) -> None: pass
 
 
-class ResourceToken:
-    def __init__(self, resource_cls: Type[IResource]) -> None:
-        self._resource_cls = resource_cls  # type: Type[IResource]
+T = TypeVar('T', bound=IResource)
 
-        self._instance = None  # type: IResource
+
+class ResourceToken(Generic[T]):
+    def __init__(self, resource_cls: Type[T]) -> None:
+        self._resource_cls = resource_cls  # type: Type[T]
+
+        self._instance = None  # type: T
         self._references = 0  # type: int
 
-        self._wrapper = self._create_resource_wrapper(resource_cls)  # type: type
+        self._wrapper = self._create_resource_wrapper(resource_cls)  # type: Type[T]
 
-    def acquire(self) -> IResource:
+    def acquire(self) -> T:
         if not self._instance:
             self._instance = self._resource_cls()
 
@@ -32,8 +35,8 @@ class ResourceToken:
             self._instance.destroy()
             self._instance = None
 
-    def _create_resource_wrapper(self, resource_cls: Type[IResource]) -> type:
-        resource_metacls = type(resource_cls)
+    def _create_resource_wrapper(self, resource_cls: Type[T]) -> Type[T]:
+        resource_metacls = type(resource_cls)  # type: Type[Type[T]]
 
         class ResourceWrapperMeta(resource_metacls):
             def __new__(cls, name: str, bases: List[type], dict_: dict, *args) -> type:
@@ -48,29 +51,26 @@ class ResourceToken:
             def __hash__(self):
                 return hash(resource_cls)
 
-            def __str__(self):
-                return str(resource_cls)
-
         class ResourceWrapper(resource_cls, metaclass=ResourceWrapperMeta):
             LOCALS = {'_target', 'token', 'release', 'released'}  # type: Set[str]
 
-            token = self  # type: ResourceToken
+            token = self  # type: ResourceToken[T]
 
             def __init__(self, target: IResource) -> None:
-                self._target = target  # type: IResource
+                self._target = target  # type: T
                 self.released = False  # type: bool
 
             def __getattribute__(self, name: str) -> Any:
                 if name in ResourceWrapper.LOCALS:
                     return object.__getattribute__(self, name)
-                else:
-                    return getattr(self._target, name)
+
+                return getattr(self._target, name)
 
             def __setattr__(self, name: str, value: Any) -> None:
                 if name in ResourceWrapper.LOCALS:
                     object.__setattr__(self, name, value)
-                else:
-                    setattr(self._target, name, value)
+
+                setattr(self._target, name, value)
 
             def release(self) -> None:
                 if self.released:
