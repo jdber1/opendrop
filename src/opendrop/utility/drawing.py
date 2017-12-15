7b4bbf975780350914ffbe26f116b10f90854454
eugenevorld <eugene_xhuang@hotmail.com>
2017-12-16 02:11:12 +0800

    added pixbuf_from_array

---
+++ src/opendrop/utility/drawing.py
@@ -0,0 +1,40 @@
+from typing import Sequence
+
+import numpy as np
+from gi.repository import GdkPixbuf, GLib
+
+
+def pixbuf_from_array(image: Sequence[Sequence[Sequence[int]]]):
+    if not isinstance(image, np.ndarray):
+        image = np.array(image)
+
+    # "Image data in 8-bit/sample packed format inside a `Glib.Bytes`"
+    data = image.astype(np.uint8).tobytes()  # type: bytes
+
+    # Colour space of image, only RGB supported currently
+    colorspace = GdkPixbuf.Colorspace.RGB  # type: GdkPixbuf.Colorspace
+
+    # If the data has an opacity channel ?
+    has_alpha = False  # type: boolean
+
+    # The size in bits of each R, G, B component?
+    bits_per_sample = 8  # type: int
+
+    # Width of the image
+    width = image.shape[1]  # type: int
+
+    # Height of the image
+    height = image.shape[0]  # type: int
+
+    # Basically the size of each row in bytes
+    rowstride = width * bits_per_sample/8 * 3  # type: int
+
+    return GdkPixbuf.Pixbuf.new_from_bytes(
+        GLib.Bytes(data),
+        colorspace,
+        has_alpha,
+        bits_per_sample,
+        width,
+        height,
+        rowstride
+    )
---
+++ tests/utility/test_drawing.py
@@ -0,0 +1,25 @@
+import os
+
+import cv2
+import gi
+
+gi.require_version('GdkPixbuf', '2.0')
+
+from gi.repository import GdkPixbuf
+
+from opendrop.utility.drawing import pixbuf_from_array
+from tests import samples
+
+SAMPLES_DIR = os.path.dirname(samples.__file__)
+
+TEST_IMAGE_PATH = os.path.join(SAMPLES_DIR, 'images', 'image0.png')
+
+
+def test_pixbuf_from_array():
+    im = cv2.imread(TEST_IMAGE_PATH)
+
+    pixbuf = pixbuf_from_array(im)
+
+    assert isinstance(pixbuf, GdkPixbuf.Pixbuf)
+
+    assert im.tobytes() == pixbuf.get_pixels()
