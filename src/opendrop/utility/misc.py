5665f3633f2bc42a0909af51ba1a76fa8c650d08
eugenevorld <eugene_xhuang@hotmail.com>
2018-01-08 00:38:38 +0800

    changed behaviour of recursive_load and added get_classes_in_modules

--- src/opendrop/utility/misc.py
+++ src/opendrop/utility/misc.py
@@ -1,20 +1,43 @@
 import importlib
+import inspect
 import pkgutil
 from enum import Enum
 from types import ModuleType
-from typing import Union, Optional, Any
+from typing import Union, Optional, Any, Type, List, Iterable, TypeVar
+
+T = TypeVar('T')
 
 
-def recursive_load(pkg: Union[ModuleType, str]) -> None:
-    pkg = importlib.import_module(pkg) if isinstance(pkg, str) else pkg
+def recursive_load(pkg: Union[ModuleType, str]) -> List[ModuleType]:
+    pkg = importlib.import_module(pkg) if isinstance(pkg, str) else pkg  # type: ModuleType
+
+    loaded_modules = [pkg]  # type: List[ModuleType]
 
     if hasattr(pkg, '__path__'):
-        for loader, name, is_pkg in pkgutil.walk_packages(pkg.__path__):
+        for loader, name, is_pkg in pkgutil.iter_modules(pkg.__path__):
             full_name = pkg.__name__ + '.' + name
-
             child = importlib.import_module(full_name)
+            loaded_modules += recursive_load(child)
 
-            recursive_load(child)
+    return loaded_modules
+
+
+def get_classes_in_modules(m: Union[Iterable[ModuleType], ModuleType], cls: T) -> List[T]:
+    clses = []  # type: List[Type]
+
+    if isinstance(m, Iterable):
+        for v in m:
+            clses += get_classes_in_modules(v, cls)
+
+        return clses
+
+    for name in dir(m):
+        attr = getattr(m, name)
+
+        if inspect.isclass(attr) and issubclass(attr, cls) and attr.__module__ == m.__name__:
+            clses.append(attr)
+
+    return clses
 
 
 # No longer used by anything, probably delete in the future
--- tests/utility/test_misc.py
+++ tests/utility/test_misc.py
@@ -2,17 +2,34 @@ import sys
 
 from pytest import raises
 
-from opendrop.utility.misc import recursive_load, EnumBuilder
+from opendrop.utility.misc import recursive_load, EnumBuilder, get_classes_in_modules
 from tests.samples import dummy_pkg
 
 
+def test_get_classes_in_modules():
+    clses = get_classes_in_modules(recursive_load(dummy_pkg), dummy_pkg.DummyClass)
+
+    assert set(clses) == {
+        dummy_pkg.DummyClass,
+        dummy_pkg.module_a.MyFirstClass,
+        dummy_pkg.subpkg.module_b.MySecondClass
+    }
+
+
 def test_recursive_load():
-    recursive_load(dummy_pkg)
+    modules = recursive_load(dummy_pkg)
 
     assert all(
         k in sys.modules
         for k in ['tests.samples.dummy_pkg.module_a', 'tests.samples.dummy_pkg.subpkg.module_b']
     )
+
+    assert set(modules) == {
+        dummy_pkg,
+        dummy_pkg.module_a,
+        dummy_pkg.subpkg,
+        dummy_pkg.subpkg.module_b
+    }
 
 
 def test_enum_builder():
