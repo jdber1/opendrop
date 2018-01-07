3ddfba4596cd806c8fc61322d478fb2ec3b3a5f7
eugenevorld <eugene_xhuang@hotmail.com>
2018-01-08 00:21:17 +0800

    added strategy

---
+++ src/opendrop/utility/strategy.py
@@ -0,0 +1,125 @@
+import inspect
+import types
+import weakref
+from abc import abstractmethod
+from typing import Callable, Optional, Any, Tuple, List
+
+
+class Strategy:
+    def __init__(self, decorator: Callable[[Callable], Callable], descriptor_args: Optional[Tuple[Any, Any]] = None)\
+            -> None:
+        self._decorator = decorator
+        self._descriptor_args = descriptor_args
+
+    def __call__(self, *args, **kwargs) -> Any:
+        f = self._do_descriptor_protocol(self._get_impl())  # type: Callable
+        return f(*args, **kwargs)
+
+    def use(self, f: Callable) -> None:
+        # Save a copy for error reporting
+        original_f = f  # type: Callable
+
+        if self._decorator is not None:
+            f = self._decorator(f)
+
+        f_sig = inspect.signature(self._do_descriptor_protocol(f))  # type: inspect.Signature
+
+        if not self.check_signature_compatible(f_sig):
+            raise ValueError
+
+        self._set_impl(f)
+
+    def clear(self) -> None:
+        self._set_impl(None)
+
+    def _do_descriptor_protocol(self, f: Callable) -> Callable:
+        if self._descriptor_args is not None and hasattr(f, '__get__'):
+            f = f.__get__(*self._descriptor_args)
+
+        return f
+
+    def check_signature_compatible(self, f_sig: inspect.Signature) -> bool:
+        # Signature checking of strategy implementation not yet implemented, so any strategy may use any
+        # implementation, even if signatures are incompatible, e.g.
+        #
+        #     @strategy
+        #     def test(a):
+        #         pass
+        #
+        #     def impl(a, b, c):
+        #         return a + b + c
+        #
+        #     test.use(impl)  # Currently works
+        #
+        # TODO: Implement this
+
+        my_sig = inspect.signature(self._do_descriptor_protocol(self._get_impl()))  # type: inspect.Signature
+
+        return True
+
+
+
+    @abstractmethod
+    def _get_impl(self) -> Callable: pass
+
+    @abstractmethod
+    def _set_impl(self, value: Optional[Callable]) -> None: pass
+
+
+class UnboundStrategy(Strategy):
+    def __init__(self, default: Callable):
+        decorator = None
+
+        if isinstance(default, classmethod):
+            decorator = classmethod
+        elif isinstance(default, staticmethod):
+            decorator = staticmethod
+
+        super().__init__(decorator)
+
+        self._default = default  # type: Callable
+        self._custom = None  # type: Optional[Callable]
+
+        self._bounded_strategies = {}
+
+    def __get__(self, instance: Any, owner: type):
+        if instance is None:
+            # Convert to a 'class strategy'
+            self._descriptor_args = (None, owner)
+
+            return self
+
+        if id(instance) not in self._bounded_strategies:
+            self._bounded_strategies[id(instance)] = InstanceStrategy(self, instance)
+
+            def callback(instance):
+                del self._bounded_strategies[id(instance)]
+
+            weakref.ref(instance, callback)
+
+        return self._bounded_strategies[id(instance)]
+
+    def _get_impl(self) -> Callable:
+        return self._custom if self._custom is not None else self._default
+
+    def _set_impl(self, value: Optional[Callable]) -> None:
+        self._custom = value
+
+
+class InstanceStrategy(Strategy):
+    def __init__(self, parent: Strategy, instance: Any):
+        super().__init__(parent._decorator, (instance, type(instance)))
+
+        self._parent = parent  # type: UnboundStrategy
+
+        self._custom = None  # type: Optional[Callable]
+
+    def _get_impl(self) -> Callable:
+        return self._custom if self._custom is not None else self._parent._get_impl()
+
+    def _set_impl(self, value: Optional[Callable]) -> None:
+        self._custom = value
+
+
+def strategy(f: Callable) -> Strategy:
+    return UnboundStrategy(f)
---
+++ tests/utility/test_strategy.py
@@ -0,0 +1,152 @@
+import pytest
+from pytest import raises
+
+from opendrop.utility.strategy import strategy
+
+def test_stub(): ...
+
+@pytest.mark.skip
+def test_positional_only_impl():
+    @strategy
+    def my_hook(a, b):
+        return 0
+
+    with raises(ValueError):
+        my_hook.use(pow)
+
+
+@pytest.mark.skip
+class TestStrategy:
+    def setup(self):
+        @strategy
+        def my_hook(a, b, c, *, e=0):
+            return 0
+
+        def my_impl(a, b, c):
+            return a + b + c
+
+        def my_impl2(a, b, c, d=1, e=10, f=100):
+            return a + b + c + d + e
+
+        self.my_hook = my_hook
+        self.my_impl = my_impl
+        self.my_impl2 = my_impl2
+
+    def test_default(self):
+        assert self.my_hook(1, 2, 3) == 0
+
+    def test_use(self):
+        self.my_hook.use(self.my_impl)
+
+        assert self.my_hook(1, 2, 3) == self.my_impl(1, 2, 3)
+
+    def test_clear(self):
+        assert self.my_hook(1, 2, 3) == 0
+
+        self.my_hook.use(self.my_impl)
+
+        assert self.my_hook(1, 2, 3) == self.my_impl(1, 2, 3)
+
+        self.my_hook.clear()
+
+        assert self.my_hook(1, 2, 3) == 0
+
+    def test_use_wrong_signature(self):
+        def my_wrong_impl(a, b, c, d): pass
+
+        with raises(ValueError):
+            self.my_hook.use(my_wrong_impl)
+
+        def my_wrong_impl2(x, y, z): pass
+
+        with raises(ValueError):
+            self.my_hook.use(my_wrong_impl2)
+
+    def test_use_compatible(self):
+        self.my_hook.use(self.my_impl2)
+
+        assert self.my_hook(1, 2, 3) == self.my_impl2(1, 2, 3)
+
+
+@pytest.mark.skip
+class TestBoundStrategy:
+    def setup(self):
+        class A:
+            @strategy
+            def my_hook1(self, a, b, c):
+                return self, 0
+
+            @strategy
+            @classmethod
+            def my_hook2(cls, a, b, c):
+                return cls, 0
+
+            @strategy
+            @staticmethod
+            def my_hook3(a, b, c):
+                return None, 0
+
+        def my_impl1(self, a, b, c):
+            return self, a + b + c
+
+        def my_impl2(self, a, b, c):
+            return self, 2 * (a+b+c)
+
+        def my_impl3(a, b, c):
+            return None, 3 * (a+b+c)
+
+        self.A = A
+        self.my_impl1 = my_impl1
+        self.my_impl2 = my_impl2
+        self.my_impl3 = my_impl3
+
+    def test_default(self):
+        a = self.A()
+
+        lret, rret = a.my_hook1(1, 2, 3)
+
+        assert lret == a and rret == 0
+
+    def test_use(self):
+        a1 = self.A()
+        a2 = self.A()
+
+        a1.my_hook1.use(self.my_impl1)
+        a2.my_hook1.use(self.my_impl2)
+
+        lret1, rret1 = a1.my_hook1(1, 2, 3)
+        lret2, rret2 = a2.my_hook1(1, 2, 3)
+
+        assert lret1 == a1 and rret1 == 6
+        assert lret2 == a2 and rret2 == 12
+
+    def test_use_on_class_strategy(self):
+        A = self.A
+        a = self.A()
+
+        A.my_hook1.use(self.my_impl1)
+
+        assert a.my_hook1(1, 2, 3) == self.my_impl1(a, 1, 2, 3)
+
+    def test_classmethod_strategy(self):
+        A = self.A
+        a = self.A()
+
+        A.my_hook2.use(self.my_impl2)
+
+        assert A.my_hook2(1, 2, 3) == a.my_hook2(1, 2, 3) == self.my_impl2(A, 1, 2, 3)
+
+    def test_staticmethod_strategy(self):
+        A = self.A
+        a = self.A()
+
+        A.my_hook3.use(self.my_impl3)
+
+        lret1, rret1 = A.my_hook3(1, 2, 3)
+        lret2, rret2 = a.my_hook3(1, 2, 3)
+
+        assert A.my_hook3(1, 2, 3) == a.my_hook3(1, 2, 3) == self.my_impl3(1, 2, 3)
+
+    def test_use_wrong_signature(self):
+        with raises(ValueError):
+            self.A.my_hook1.use(self.my_impl3)
