a41a692c44c66fdbc4da9fc784594157c896f98e
eugenevorld <eugene_xhuang@hotmail.com>
2018-01-08 11:33:16 +0800

    changed test to reflect refactoring

--- tests/mvp/test_view.py
+++ tests/mvp/test_view.py
@@ -6,34 +6,16 @@ import pytest
 from tests.mvp.samples import MyView, OtherView
 
 
-@pytest.mark.asyncio
-async def test_view_close():
+def test_view_close():
     view = MyView()
 
-    handle_close = Mock(spec=[])
-
-    view.connect('on_close', handle_close)
-
-    view.close(OtherView)
-
-    await asyncio.sleep(0)
-
-    handle_close.assert_called_once_with(view, OtherView)
+    view.close()
 
 
-@pytest.mark.asyncio
-async def test_view_spawn():
+def test_view_spawn():
     view = MyView()
 
-    handle_spawn = Mock(spec=[])
-
-    view.connect('on_spawn', handle_spawn)
-
-    view.spawn(OtherView, child=False, args=(1, 2, 3), kwargs={'a': 1, 'b': 2, 'c': 3})
-
-    await asyncio.sleep(0)
-
-    handle_spawn.assert_called_once_with(view, OtherView, False, (1, 2, 3), {'a': 1, 'b': 2, 'c': 3})
+    view.spawn(OtherView, child=False, view_opts={'a': 1, 'b': 2, 'c': 3})
 
 
 @pytest.mark.asyncio
