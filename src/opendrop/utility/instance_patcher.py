248ecd7133da15643cde31bab920100c2960cf80
eugenevorld <eugene_xhuang@hotmail.com>
2018-01-14 13:55:41 +0800

    improved doc

--- src/opendrop/utility/instance_patcher.py
+++ src/opendrop/utility/instance_patcher.py
@@ -4,9 +4,47 @@ T = TypeVar('T')
 U = TypeVar('U')
 
 
-# Limitations: object.__set/getattribute__ won't work correctly on a PatchedInstance, PatchedInstance's also won't equal
-# the instance that was patched, and may have a different hash unless a __hash__ is implemented in a way that makes them
-# equal.
+# Limitations: type.__set/getattribute__ won't work correctly on PatchedInstance classes:
+#
+#     class A:
+#         pass
+#
+#     class APatch:
+#         @classmethod
+#         def test(cls):
+#             type.__setattr__(cls, 'test', 10)
+#
+#     a = A()
+#     a_patched = patch_instance(a)
+#
+#     a_patched.test()
+#
+#     print(type.__getattribute__(type(a_patched), 'test'))  # 10
+#     print(type.__getattribute__(A, 'test'))                # AttributeError: type object 'A' has no attribute 'temp'
+#     print(type(a_patched).test)                            # AttributeError: type object 'A' has no attribute 'temp'
+#     print(A.test)                                          # AttributeError: type object 'A' has no attribute 'temp'
+#
+# Instances (and classes) of PatchedInstance also won't equal the original instance/class:
+#
+#     a = A()
+#     a_patched = patch_instance(a)
+#
+#     print(a == a_patched)  # False
+#     print(A == type(a_patched))  # False
+#
+# They may also have a different hash unless a __hash__ is implemented in a way that makes them equal.
+#
+# PatchedInstance instances are implemented by setting the __dict__ attribute of the patched instance to that of the
+# source instance. The patched instances' class (i.e. `type(patched_instance)`) has a metaclass overriding
+# __getattribute__ and __setattr__ for redirecting attribute accesses to the source instance class, this is not a
+# completely transparent process but will behave as expected for most usecases. The source instance class and mixin
+# class's metaclass __getattribute__ and __setattr__ are ignored as a byproduct of this implementation. Also, attribute
+# access to the metaclass of a patched instance are not redirected to the metaclass of the source instance, and neither
+# are the metaclasses of metaclasses.
+#
+# Basically, attribute access works for patched instances, works most of the time
+# for classes of patched instances, and do not work for its metaclasses or metaclasses of metaclasses)
+
 def patch_instance(source: T, mixin: Type[U]) -> U:
     source_cls = type(source)  # type: Type[T]
 
