685d4874b80992d395cb6d5bd3aabdd6b19e914a
eugenevorld <eugene_xhuang@hotmail.com>
2018-01-14 13:55:10 +0800

    refactored, and class attr access redirected

--- src/opendrop/utility/instance_patcher.py
+++ src/opendrop/utility/instance_patcher.py
@@ -8,36 +8,50 @@ U = TypeVar('U')
 # the instance that was patched, and may have a different hash unless a __hash__ is implemented in a way that makes them
 # equal.
 def patch_instance(source: T, mixin: Type[U]) -> U:
-    class PatchedInstance(mixin):
-        def __new__(cls, *args, **kwargs):
-            return object.__new__(cls)
+    source_cls = type(source)  # type: Type[T]
 
-        def __init__(self):
-            pass
+    inherit = type(mixin), type(source_cls)
 
+    if issubclass(type(source_cls), type(mixin)):
+        inherit = reversed(inherit)
+
+    class PatchedInstanceMeta(*inherit):
         def __getattribute__(self, name: str) -> Any:
+            if name in ('__new__', '__init__'):
+                return type.__getattribute__(self, name)
+
             try:
-                attr = getattr(mixin, name)
+                attr = getattr(type(mixin), name) if hasattr(type(mixin), name) else getattr(type(source_cls), name)
 
                 if hasattr(attr, '__get__'):
-                    if hasattr(attr, '__set__') or name not in source.__dict__:
-                        attr = attr.__get__(self, mixin)
+                    if hasattr(attr, '__set__') or name not in source_cls.__dict__:
+                        attr = attr.__get__(self, type(source_cls))
                     else:
-                        return source.__dict__[name]
+                        attr = source_cls.__dict__[name]
 
                 return attr
             except AttributeError:
-                return getattr(source, name)
+                return getattr(source_cls, name)
 
         def __setattr__(self, name: str, value: Any) -> None:
             try:
-                attr = getattr(mixin, name)
+                attr = getattr(type(mixin), name) if hasattr(type(mixin), name) else getattr(type(source_cls), name)
 
                 if hasattr(attr, '__set__'):
                     attr.__set__(self, value)
                 else:
                     raise AttributeError
             except AttributeError:
-                return setattr(source, name, value)
+                return setattr(source_cls, name, value)
+
+    class PatchedInstance(mixin, source_cls, metaclass=PatchedInstanceMeta):
+        def __new__(cls, *args, **kwargs):
+            obj = object.__new__(cls)
+            obj.__dict__ = source.__dict__
+
+            return obj
+
+        def __init__(self):
+            pass
 
     return PatchedInstance()
--- tests/utility/test_instance_extender.py
+++ tests/utility/test_instance_extender.py
@@ -3,7 +3,15 @@ from pytest import raises
 from opendrop.utility.instance_patcher import patch_instance
 
 
-class A:
+class AMeta(type):
+    @property
+    def cls_prop(self):
+        return 5
+
+
+class A(metaclass=AMeta):
+    cls_val = None
+
     def __init__(self, v):
         self.val = v
         self._prop = 0
@@ -18,21 +26,17 @@ class A:
         self.val = v
 
     @classmethod
-    def cls_method(cls):
-        return cls
+    def set_cls_val(cls, v):
+        cls.cls_val = v
 
     @property
     def prop(self):
         return self._prop
 
-    print('1', prop)
-
     @prop.setter
     def prop(self, v):
         print('A prop')
         self._prop = v
-
-    print('2', prop)
 
     def __add__(self, other):
         return 9
@@ -41,7 +45,7 @@ class A:
         return 3
 
 
-class A2(A):
+class A2:
     def dothis(self, v):
         return 2 * v
 
@@ -53,6 +57,10 @@ class A2(A):
 
     def set_val(self, v):
         self.val = 3 * v
+
+    @classmethod
+    def set_cls_val(cls, v):
+        cls.cls_val = 3 * v
 
     @A.prop.setter
     def prop(self, v):
@@ -86,10 +94,22 @@ class TestPatchedInstance:
 
         assert self.a.val == self.a_patched.val == o
 
+    def test_class_attribute_access(self):
+        o = object()
+
+        type(self.a_patched).cls_val = o
+
+        assert A.cls_val == type(self.a_patched).cls_val == o
+
     def test_method_self_parameter(self):
         self.a_patched.set_val(5)
 
         assert self.a.val == self.a_patched.val == 3 * 5
+
+    def test_class_method_self_parameter(self):
+        self.a_patched.set_cls_val(5)
+
+        assert A.cls_val == type(self.a_patched).cls_val == 3 * 5
 
     def test_property(self):
         self.a.prop = 1
@@ -100,9 +120,8 @@ class TestPatchedInstance:
 
         assert self.a.prop == self.a_patched.prop == 10 * 10
 
-    def test_class_method(self):
-        assert self.a.cls_method() == A
-        assert self.a_patched.cls_method() == A2
+    def test_class_property(self):
+        assert type(self.a_patched).cls_prop == 5
 
     def test_dunder(self):
         assert self.a + self.a == 9
@@ -114,3 +133,19 @@ class TestPatchedInstance:
         assert self.a_patched * self.a_patched == 3
 
         print(self.a, self.a_patched)
+
+    def test_object_setattr(self):
+        object.__setattr__(self.a_patched, 'test_os_var', 15)
+
+        assert self.a.test_os_var == self.a_patched.test_os_var == 15
+
+    def test_object_getattribute(self):
+        object.__setattr__(self.a_patched, 'test_og_var', 15)
+
+        assert \
+            object.__getattribute__(self.a, 'test_og_var') == object.__getattribute__(self.a_patched, 'test_og_var') == \
+            self.a.test_og_var == self.a_patched.test_og_var \
+            == 15
+
+    def test_misc(self):
+        print(self.a == self.a_patched)
