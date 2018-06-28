8caff8798511e9e41002ec7480d68289b93191a9
eugenevorld <eugene_xhuang@hotmail.com>
2018-06-28 21:37:38 +0930

    modified zindex behaviour

--- src/opendrop/image_filter/image_filter_group.py
+++ src/opendrop/image_filter/image_filter_group.py
@@ -1,7 +1,7 @@
-from typing import Any, Generic, TypeVar, List
+import time
+from typing import Generic, TypeVar, List
 
 import numpy as np
-import time
 
 from opendrop.image_filter.bases import ImageFilter
 
@@ -15,7 +15,7 @@ class TimestampContainer(Generic[T]):
 
 
 class ImageFilterGroup(ImageFilter):
-    """Filters sorted by z_index (highest applied first, then lowest) and if equal z_index, sort by timestamp (earliest
+    """Filters sorted by z_index (lowest applied first, then highest) and if equal z_index, sort by timestamp (earliest
     added applied first, then latest). If filter has no z_index attribute, then z_index is set to 0"""
     def __init__(self) -> None:
         self._filters = []  # type: List[TimestampContainer[ImageFilter]]
@@ -35,8 +35,10 @@ class ImageFilterGroup(ImageFilter):
         for c in list(self._filters):
             if c.value == image_filter:
                 self._filters.remove(c)
-
                 break
 
+    def clear(self) -> None:
+        self._filters = []
+
     def _resort(self) -> None:
-        self._filters.sort(key=lambda c: (-c.value.z_index if hasattr(c.value, 'z_index') else 0, c.timestamp))
+        self._filters.sort(key=lambda c: (getattr(c.value, 'z_index', 0), c.timestamp))
