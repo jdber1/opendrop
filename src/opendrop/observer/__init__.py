67f49a12976f7475f7378b03fb66bc014893c6e0
eugenevorld <eugene_xhuang@hotmail.com>
2018-12-09 22:56:59 +0800

    removed observer

--- src/opendrop/observer/__init__.py
+++
@@ -1,2 +0,0 @@
-# Import `types` so it can be accessed via `observer.types` without being manually imported.
-from . import types
--- src/opendrop/observer/gtk/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-from opendrop.utility.misc import recursive_load
-
-recursive_load(sys.modules[__name__])
-
-from .preview_viewer import PreviewViewer
-from .preview_viewer_controller import PreviewViewerController
