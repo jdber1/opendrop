261a693f4fa92a5858e8dc91189c28580d803149
eugenevorld <eugene_xhuang@hotmail.com>
2018-12-12 12:02:02 +0800

    added speaker and moderator classes

---
+++ src/opendrop/utility/speaker.py
@@ -0,0 +1,88 @@
+from typing import Generic, TypeVar, Optional, MutableMapping, Callable, List
+
+
+K = TypeVar('K')
+
+
+class Speaker(Generic[K]):
+    def __init__(self) -> None:
+        self._moderator = None  # type: Optional[Moderator[K]]
+
+    def do_activate(self) -> None:
+        """Invoked when this Speaker is to be activated."""
+
+    def do_deactivate(self) -> None:
+        """Invoked when this Speaker is to be deactivated."""
+
+    async def do_request_deactivate(self) -> bool:
+        """This method is a coroutine. This method is invoked when the Moderator is asking for permission from this
+        Speaker to deactivate, return True to prevent this Speaker from deactivating and False to approve the
+        deactivation request. After approving the deactivation request, this Speaker will be deactivated, and the
+        deactivate() method will be invoked. Default implementation returns True"""
+        return False
+
+    @property
+    def moderator(self) -> 'Optional[Moderator[K]]':
+        return self._moderator
+
+
+class Moderator(Generic[K]):
+    def __init__(self):
+        self._active_speaker = None  # type: Optional[K]
+        self._key_to_speakers = {}  # type: MutableMapping[K, Speaker]
+
+    @property
+    def active_speaker(self) -> Optional[K]:
+        """Return the key that identifies the currently active Speaker, return None if no Speaker is currently active.
+        """
+        return self._active_speaker
+
+    def add_speaker(self, key: K, spk: Speaker) -> None:
+        if key is None:
+            raise ValueError('Identifying key cannot be None')
+
+        self._key_to_speakers[key] = spk
+        spk._moderator = self
+
+    def _handle_speaker_request_activate_speaker(self, src_key: K, activate_key: K) -> None:
+        if self.active_speaker is not src_key:
+            return
+
+        self.activate_speaker(activate_key)
+
+    async def _deactivate_active_speaker(self, force: bool) -> bool:
+        """Deactivate the currently active Speaker (if it exists), returns True if successful and False otherwise. This
+        method will not update the `_active_speaker` attribute."""
+        if self._active_speaker is None:
+            return True
+
+        # The 'actual' active speaker, not the key used to identify it
+        active_speaker = self._key_to_speakers[self._active_speaker]
+
+        block = False if force else await active_speaker.do_request_deactivate()
+
+        if block:
+            return False
+
+        active_speaker.do_deactivate()
+        return True
+
+    async def activate_speaker(self, key: Optional[K], force: bool = False) -> bool:
+        """Activate the speaker identified by key, return True if desired speaker is successfully activated, and False
+        otherwise. Pass force=True to force any currently active speaker to be deactivated, preventing it from blocking
+        the deactivation. To deactivate the current active speaker only, without activating another speaker, use
+        key=None."""
+        if key is not None and key not in self._key_to_speakers:
+            raise ValueError('No speaker identified by `{}`'.format(key))
+
+        deactivation_success = await self._deactivate_active_speaker(force)
+
+        if not deactivation_success:
+            return False
+
+        if key is not None:
+            desired_speaker = self._key_to_speakers[key]
+            desired_speaker.do_activate()
+
+        self._active_speaker = key
+        return True
---
+++ tests/utility/speaker.py
@@ -0,0 +1,154 @@
+import pytest
+
+from opendrop.utility.speaker import Speaker, Moderator
+
+
+class MySpeaker(Speaker):
+    LOG_ACTIVATE = object()
+    LOG_DEACTIVATE = object()
+    LOG_REQUEST_DEACTIVATE = object()
+
+    def __init__(self):
+        Speaker.__init__(self)
+
+        # Used for testing.
+        self.log = []
+
+    def do_activate(self):
+        self.log.append(self.LOG_ACTIVATE)
+
+    def do_deactivate(self):
+        self.log.append(self.LOG_DEACTIVATE)
+
+    async def do_request_deactivate(self):
+        self.log.append(self.LOG_REQUEST_DEACTIVATE)
+
+
+class MyBlockingSpeaker(MySpeaker):
+    LOG_BLOCK_DEACTIVATION_REQUEST = object()
+
+    async def do_request_deactivate(self):
+        self.log.append(self.LOG_BLOCK_DEACTIVATION_REQUEST)
+        return True
+
+
+# Tests start here
+
+def test_speaker_moderator_value_after_instantiate():
+    my_spk = MySpeaker()
+
+    assert my_spk.moderator is None
+
+
+def test_speaker_moderator_readonly():
+    my_spk = MySpeaker()
+
+    with pytest.raises(AttributeError):
+        my_spk.moderator = 1
+
+
+def test_moderator_active_speaker_value_after_instantiate():
+    mod = Moderator()
+
+    assert mod.active_speaker is None
+
+
+def test_moderator_active_speaker_readonly():
+    mod = Moderator()
+
+    with pytest.raises(AttributeError):
+        mod.active_speaker = 1
+
+
+def test_moderator_add_speaker():
+    mod = Moderator()
+    my_spk = MySpeaker()
+
+    # Add the speaker
+    mod.add_speaker(0, my_spk)
+    assert my_spk.log == []
+    assert my_spk.moderator == mod
+
+
+def test_moderator_add_speaker_with_key_none():
+    mod = Moderator()
+    my_spk = MySpeaker()
+
+    with pytest.raises(ValueError):
+        mod.add_speaker(None, my_spk)
+
+
+@pytest.mark.asyncio
+async def test_moderator_activate_speaker():
+    mod = Moderator()
+    my_spk = MySpeaker()
+    mod.add_speaker(0, my_spk)
+
+    # Activate the speaker
+    success = await mod.activate_speaker(0)
+
+    assert success
+    assert my_spk.log == [MySpeaker.LOG_ACTIVATE]
+    assert mod.active_speaker == 0
+
+
+@pytest.mark.asyncio
+async def test_moderator_activate_nonexistent_speaker():
+    mod = Moderator()
+
+    with pytest.raises(ValueError):
+        await mod.activate_speaker(0)
+
+
+@pytest.mark.asyncio
+async def test_moderator_deactivate_active_speaker():
+    mod = Moderator()
+    my_spk = MySpeaker()
+    mod.add_speaker(0, my_spk)
+    await mod.activate_speaker(0)
+
+    # Deactivate the current active speaker
+    await mod.activate_speaker(None)
+
+    assert my_spk.log == [MySpeaker.LOG_ACTIVATE, MySpeaker.LOG_REQUEST_DEACTIVATE, MySpeaker.LOG_DEACTIVATE]
+    assert mod.active_speaker is None
+
+
+@pytest.mark.asyncio
+async def test_moderator_activate_speaker_but_current_active_speaker_blocks():
+    mod = Moderator()
+    my_blocking_spk = MyBlockingSpeaker()
+    my_spk = MySpeaker()
+    mod.add_speaker(0, my_blocking_spk)
+    mod.add_speaker(1, my_spk)
+
+    # Activate the blocking speaker
+    await mod.activate_speaker(0)
+
+    # Try to activate another speaker
+    success = await mod.activate_speaker(0)
+
+    assert success is False
+    assert my_blocking_spk.log == [MyBlockingSpeaker.LOG_ACTIVATE, MyBlockingSpeaker.LOG_BLOCK_DEACTIVATION_REQUEST]
+    assert my_spk.log == []
+    assert mod.active_speaker == 0
+
+
+@pytest.mark.asyncio
+async def test_moderator_activate_speaker_with_force():
+    mod = Moderator()
+    my_blocking_spk = MyBlockingSpeaker()
+    my_spk = MySpeaker()
+    mod.add_speaker(0, my_blocking_spk)
+    mod.add_speaker(1, my_spk)
+
+    # Activate the blocking speaker
+    await mod.activate_speaker(0)
+
+    # Try to activate another speaker
+    success = await mod.activate_speaker(1, force=True)
+
+    assert success
+    assert my_blocking_spk.log == [MyBlockingSpeaker.LOG_ACTIVATE, MyBlockingSpeaker.LOG_DEACTIVATE]
+    assert my_spk.log == [MySpeaker.LOG_ACTIVATE]
+    assert mod.active_speaker == 1
