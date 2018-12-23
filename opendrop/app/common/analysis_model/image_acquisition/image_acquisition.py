669407390a11a30ecb7f1a82bcab9f72111773ca
eugenevorld <eugene_xhuang@hotmail.com>
2018-12-23 16:06:06 +0800

    refactored

--- opendrop/app/common/analysis_model/image_acquisition/image_acquisition.py
+++ opendrop/app/common/analysis_model/image_acquisition/image_acquisition.py
@@ -1,6 +1,6 @@
 from asyncio import Future
 from enum import Enum
-from typing import Generic, TypeVar, Type, Callable, Optional, Tuple, List, Any, Sequence
+from typing import Generic, TypeVar, Type, Callable, Optional, Tuple, Any, Sequence
 
 from opendrop.mytypes import Image
 from opendrop.utility.bindable.bindable import AtomicBindable, AtomicBindableAdapter, AtomicBindablePropertyAdapter
@@ -12,12 +12,11 @@ class ImageAcquisitionImplType(Enum):
 
 
 class ImageAcquisitionImpl:
-    def acquire_images(self) -> Tuple[List[Future], List[float]]:
+    def acquire_images(self) -> Tuple[Sequence[Future], Sequence[float]]:
         """Implementation of acquire_images()"""
 
     def create_preview(self) -> Tuple[AtomicBindable[Image], Any]:
-        """Return a tuple, first element is an AtomicBindable that provides the current preview image and the second
-        element is a configuration object that is used to configure the first element."""
+        """Implementation of create_preview()"""
 
 
 T = TypeVar('T', bound=ImageAcquisitionImplType)
@@ -28,9 +27,9 @@ class ImageAcquisition(Generic[T]):
         self._impl = None  # type: Optional[ImageAcquisitionImpl]
         self.bn_impl = AtomicBindableAdapter(self._get_impl)  # type: AtomicBindable[Optional[ImageAcquisitionImpl]]
 
-    def acquire_images(self) -> Tuple[List[Future], List[float]]:
-        """Return a tuple, with the first element being a list of futures which will be resolved to each image, and the
-        second element being a list of estimated timestamps for when the futures will be resolved."""
+    def acquire_images(self) -> Tuple[Sequence[Future], Sequence[float]]:
+        """Return a tuple, with the first element being a sequence of futures which will be resolved to each image, and
+        the second element being a sequence of estimated timestamps for when the futures will be resolved."""
         if self.impl is None:
             raise ValueError('No implementation chosen yet')
 
@@ -42,6 +41,8 @@ class ImageAcquisition(Generic[T]):
         return futs, tims
 
     def create_preview(self) -> Tuple[AtomicBindable[Image], Any]:
+        """Return a tuple, first element is an AtomicBindable that provides the current preview image and the second
+        element is a configuration object that is used to configure the first element."""
         if self.impl is None:
             raise ValueError('No implementation chosen yet')
 
