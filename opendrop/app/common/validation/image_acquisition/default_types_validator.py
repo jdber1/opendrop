ecfdab0b1bee2b99dae30ae0bb73f6d634de8956
eugenevorld <eugene_xhuang@hotmail.com>
2018-12-28 21:37:24 +0800

    added dependency injection stuff

--- opendrop/app/common/validation/image_acquisition/default_types_validator.py
+++ opendrop/app/common/validation/image_acquisition/default_types_validator.py
@@ -1,8 +1,34 @@
+from typing import TypeVar, Callable, Type, MutableMapping
+
 from opendrop.app.common.analysis_model.image_acquisition.default_types import BaseImageSequenceImageAcquisitionImpl, \
-    BaseCameraImageAcquisitionImpl
+    BaseCameraImageAcquisitionImpl, LocalImagesImageAcquisitionImpl, USBCameraImageAcquisitionImpl
+from opendrop.app.common.analysis_model.image_acquisition.image_acquisition import ImageAcquisitionImpl
 from opendrop.app.common.validation.image_acquisition.validator import ImageAcquisitionImplValidator
 
+# New types
 
+ValidatorFactory = Callable[[ImageAcquisitionImpl], ImageAcquisitionImplValidator]
+
+# Dependency injection stuff
+
+impl_to_subvalidator_factory = {}  # type: MutableMapping[Type[ImageAcquisitionImpl], ValidatorFactory]
+
+
+T = TypeVar('T', bound=ValidatorFactory)
+def this_can_validate(impl: Type[ImageAcquisitionImpl]) -> Callable[[T], T]:
+    def actual(validator_factory: T) -> T:
+        impl_to_subvalidator_factory[impl] = validator_factory
+        return validator_factory
+    return actual
+
+
+def create_subvalidator_for_impl(impl: ImageAcquisitionImpl) -> ImageAcquisitionImplValidator:
+    return impl_to_subvalidator_factory[type(impl)](impl)
+
+
+# Main classes start here
+
+@this_can_validate(LocalImagesImageAcquisitionImpl)
 class BaseImageSequenceImageAcquisitionImplValidator(ImageAcquisitionImplValidator):
     def __init__(self, target: BaseImageSequenceImageAcquisitionImpl) -> None:
         self._target = target
@@ -20,6 +46,7 @@ class BaseImageSequenceImageAcquisitionImplValidator(ImageAcquisitionImplValidat
         return True
 
 
+@this_can_validate(USBCameraImageAcquisitionImpl)
 class BaseCameraImageAcquisitionImplValidator(ImageAcquisitionImplValidator):
     def __init__(self, target: BaseCameraImageAcquisitionImpl) -> None:
         self._target = target
