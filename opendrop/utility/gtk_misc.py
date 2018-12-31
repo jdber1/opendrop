f04fb29e24073e01f18375f02e2b6400b0f4b05b
eugenevorld <eugene_xhuang@hotmail.com>
2018-12-31 12:14:21 +0800

    added assertion that image has three channels, changed boolean to bool in type hint.

--- opendrop/utility/gtk_misc.py
+++ opendrop/utility/gtk_misc.py
@@ -8,6 +8,9 @@ def pixbuf_from_array(image: Sequence[Sequence[Sequence[int]]]):
     if not isinstance(image, np.ndarray):
         image = np.array(image)
 
+    # Assert that `image` has three channels
+    assert len(image.shape) == 3 and image.shape[-1] == 3
+
     # "Image data in 8-bit/sample packed format inside a `Glib.Bytes`"
     data = image.astype(np.uint8).tobytes()  # type: bytes
 
@@ -15,7 +18,7 @@ def pixbuf_from_array(image: Sequence[Sequence[Sequence[int]]]):
     colorspace = GdkPixbuf.Colorspace.RGB  # type: GdkPixbuf.Colorspace
 
     # If the data has an opacity channel ?
-    has_alpha = False  # type: boolean
+    has_alpha = False  # type: bool
 
     # The size in bits of each R, G, B component?
     bits_per_sample = 8  # type: int
