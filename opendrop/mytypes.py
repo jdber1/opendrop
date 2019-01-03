1080b292c19e16f2145151b3b9e9c311872af861
eugenevorld <eugene_xhuang@hotmail.com>
2019-01-03 18:55:27 +0800

    made Destroyable a protocol

--- opendrop/mytypes.py
+++ opendrop/mytypes.py
@@ -1,8 +1,11 @@
+from typing import TypeVar, Generic, overload, Tuple, Any
+
 import numpy as np
+from typing_extensions import Protocol
 
 Image = np.ndarray
 
 
-class Destroyable:
+class Destroyable(Protocol):
     def destroy(self) -> None:
         """Destroy this object"""
