9a6217d4e9e6e07489726ad785a8d4d0398d40a8
eugenevorld <eugene_xhuang@hotmail.com>
2019-01-18 17:28:30 +0800

    added bindable function

--- opendrop/utility/bindable/__init__.py
+++ opendrop/utility/bindable/__init__.py
@@ -1,3 +1,3 @@
 from .bindable import AtomicBindable, AtomicBindableAdapter, AtomicBindableVar
 from .set import SetBindable, ModifiableSetBindable, BuiltinSetBindable
-from .experimental import if_expr
+from .experimental import if_expr, bindable_function
--- opendrop/utility/bindable/experimental.py
+++ opendrop/utility/bindable/experimental.py
@@ -1,9 +1,11 @@
-from typing import TypeVar, Optional, Sequence
+import itertools
+from typing import TypeVar, Optional, Sequence, Callable, Mapping, Union, Any
 
-from .bindable import AtomicBindable, Bindable
-
+from opendrop.utility.bindable import SetBindable
+from .bindable import AtomicBindable, Bindable, BaseAtomicBindable
 
 TxT = TypeVar('TxT')
+VT = TypeVar('VT')
 
 
 class BindableProxy(Bindable[TxT]):
@@ -44,3 +46,56 @@ class IfExpr(BindableProxy[TxT]):
 
 
 if_expr = IfExpr
+
+
+# AtomicBindableFunction
+
+ArgType = Union[AtomicBindable, SetBindable]
+
+
+class AtomicFunctionReturnBindable(BaseAtomicBindable[VT]):
+    def __init__(self, func: Callable[..., VT], args: Sequence[ArgType], kwargs: Mapping[str, ArgType]) -> None:
+        super().__init__()
+        self._func = func
+        self._args = tuple(args)
+        self._kwargs = dict(kwargs)
+        self._result = self._calculate_result()
+
+        for x in itertools.chain(self._args, self._kwargs.values()):
+            if not isinstance(x, Bindable):
+                continue
+            x.on_new_tx.connect(self._update_result, ignore_args=True)
+
+    @staticmethod
+    def _transform_to_pod(x: ArgType) -> Any:
+        if not isinstance(x, Bindable):
+            return x
+        bindable = x
+        if isinstance(bindable, AtomicBindable):
+            return bindable.get()
+        elif isinstance(bindable, SetBindable):
+            return set(bindable)
+        else:
+            raise ValueError('Unrecognized bindable type')
+
+    def _calculate_result(self) -> VT:
+        args_pod = [self._transform_to_pod(x) for x in self._args]
+        kwargs_pod = {k: self._transform_to_pod(v) for k, v in self._kwargs}
+        return self._func(*args_pod, **kwargs_pod)
+
+    def _update_result(self) -> None:
+        self._result = self._calculate_result()
+        self.poke()
+
+    def _raw_get(self) -> VT:
+        return self._result
+
+    def _raw_set(self, value: Any):
+        raise AttributeError("Can't set")
+
+
+def bindable_function(func: Callable) -> Callable:
+    def wrapper(*args, **kwargs):
+        return AtomicFunctionReturnBindable(func, args, kwargs)
+
+    return wrapper
