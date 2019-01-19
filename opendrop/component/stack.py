25680ca5cadfc89c9ba4fb87e224827fff50cf26
eugenevorld <eugene_xhuang@hotmail.com>
2019-01-19 11:43:26 +0800

    removed useless imports

--- opendrop/component/stack.py
+++ opendrop/component/stack.py
@@ -3,8 +3,7 @@ from typing import Generic, TypeVar, MutableMapping, Optional, Sequence
 from gi.repository import Gtk
 
 from opendrop.component.gtk_widget_view import GtkWidgetView
-from opendrop.utility.bindable.bindable import BaseAtomicBindable, AtomicBindableAdapter, AtomicBindable
-from opendrop.utility.bindable.binding import Binding, AtomicBindingMITM
+from opendrop.utility.bindable.bindable import AtomicBindableAdapter, AtomicBindable
 from opendrop.utility.events import Event
 
 KeyType = TypeVar('KeyType')
