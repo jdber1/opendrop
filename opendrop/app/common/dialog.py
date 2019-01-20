60dbda4f4744e886b52a168f398dc691fcf723aa
eugenevorld <eugene_xhuang@hotmail.com>
2019-01-20 15:58:01 +0800

    added ErrorDialogView

--- opendrop/app/common/dialog.py
+++ opendrop/app/common/dialog.py
@@ -78,3 +78,38 @@ class YesNoDialogPresenter:
         for f in self.__cleanup_tasks:
             f()
         self.__destroyed = True
+
+
+class ErrorDialogView(GtkWidgetView[Gtk.Dialog]):
+    def __init__(self, message: str, window: Optional[Gtk.Window]) -> None:
+        self.widget = Gtk.MessageDialog(
+            parent=window,
+            flags=Gtk.DialogFlags.MODAL | Gtk.DialogFlags.DESTROY_WITH_PARENT,
+            type=Gtk.MessageType.ERROR,
+            buttons=Gtk.ButtonsType.CLOSE,
+            message_format=message)
+        self.widget.show()
+
+        # Add a reference to self in the widget, otherwise self gets garbage collected for some reason.
+        self.widget.__ref_to_view = self
+
+        # Wiring things up
+
+        self.on_acknowledged = Event()
+
+        self.widget.connect('response', self._hdl_widget_response)
+        self.widget.connect('delete-event', self._hdl_widget_delete_event)
+        self.widget.connect('destroy', self._hdl_widget_destroy)
+
+    def _hdl_widget_response(self, widget: Gtk.Dialog, response: Gtk.ResponseType) -> None:
+        self.on_acknowledged.fire()
+
+    def _hdl_widget_delete_event(self, widget: Gtk.Dialog, event: Gdk.Event) -> bool:
+        # return True to block the dialog from closing.
+        return True
+
+    def _hdl_widget_destroy(self, widget: Gtk.Dialog) -> None:
+        self.widget.emit('response', Gtk.ResponseType.DELETE_EVENT)
+
+    def destroy(self) -> None:
+        self.widget.destroy()
