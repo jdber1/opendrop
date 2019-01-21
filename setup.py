17eb5c8b88551054d538f8de7026046ac51d5186
eugenevorld <eugene_xhuang@hotmail.com>
2019-01-22 01:12:25 +0800

    updated setup.py to a basic working version for testing

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="opendrop",
-    version="2.0dev",
-    packages=find_packages(exclude=["opendrop.bin"]),
-    include_package_data=True,
-    entry_points={"console_scripts": [
-        "opendrop=opendrop.app:main"
-    ]},
-    install_requires=[]
+    name='opendrop',
+    version='2.0.0dev',
+    packages=find_packages(exclude=['tests', 'manual_tests', 'docs']),
+    package_data={
+        'opendrop.res': ['images/*']
+    },
+    entry_points={
+        'console_scripts': 'opendrop=opendrop.app:main'
+    },
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'scipy',
+        'pycairo',
+        'pygobject',
+        'pytest',
+        'setuptools',
+        'typing_extensions',
+    ]
 )
