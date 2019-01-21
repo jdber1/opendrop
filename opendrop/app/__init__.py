17c364fc470af69b4de1f30ec4d65511c3f4cb1f
eugenevorld <eugene_xhuang@hotmail.com>
2019-01-21 18:24:13 +0800

    moved imports into main()

--- opendrop/app/__init__.py
+++ opendrop/app/__init__.py
@@ -1,11 +1,4 @@
 def main():
-    glibcoro.install()
-    default_app_speakers_factory = DefaultAppSpeakersFactory()
-    app = App(default_app_speakers_factory)
-    app.run()
-
-
-if __name__ == '__main__':
     import gi
     gi.require_version('Gtk', '3.0')
     gi.require_version('Gdk', '3.0')
@@ -15,4 +8,11 @@ if __name__ == '__main__':
 
     from opendrop.vendor.glibcoro import glibcoro
 
+    glibcoro.install()
+    default_app_speakers_factory = DefaultAppSpeakersFactory()
+    app = App(default_app_speakers_factory)
+    app.run()
+
+
+if __name__ == '__main__':
     main()
