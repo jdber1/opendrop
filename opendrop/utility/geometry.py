b9718865160be64be65e173151a37ab81f48151a
eugenevorld <eugene_xhuang@hotmail.com>
2019-01-28 15:26:23 +0800

    Implement Vector2 class

--- opendrop/utility/geometry.py
+++ opendrop/utility/geometry.py
@@ -1,9 +1,87 @@
-from typing import Generic, TypeVar, Tuple, Callable, Iterator, Any, overload
+from numbers import Number
+from typing import Generic, TypeVar, Tuple, Callable, Iterator, Any, overload, Iterable, Union
 
-NumericType = TypeVar('NT', int, float, complex)
-SomeNumericType = TypeVar('SomeNumericType', int, float, complex)
+NumericType = TypeVar('NT', bound=Number)
+SomeNumericType = TypeVar('SomeNumericType', bound=Number)
 
-Vector2 = Tuple[float, float]
+
+class Vector2(Generic[NumericType]):
+    def __init__(self, x: NumericType, y: NumericType) -> None:
+        self._x = x
+        self._y = y
+
+    @property
+    def x(self) -> NumericType:
+        return self._x
+
+    @property
+    def y(self) -> NumericType:
+        return self._y
+
+    def as_type(self, cast: Callable[[NumericType], SomeNumericType]) -> 'Vector2[SomeNumericType]':
+        return Vector2(cast(self.x), cast(self.y))
+
+    def __neg__(self) -> 'Vector2[NumericType]':
+        return Vector2(-self.x, -self.y)
+
+    def __add__(self, other: Iterable[SomeNumericType]) -> 'Vector2':
+        try:
+            other = self._ensure_vector2(other)
+        except TypeError:
+            return NotImplemented
+
+        return Vector2(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: Iterable[SomeNumericType]) -> 'Vector2':
+        try:
+            other = self._ensure_vector2(other)
+        except TypeError:
+            return NotImplemented
+
+        return self + (-other)
+
+    @classmethod
+    def _ensure_vector2(cls, obj: Iterable[SomeNumericType]) -> 'Vector2[SomeNumericType]':
+        if isinstance(obj, cls):
+            return obj
+
+        if not isinstance(obj, Iterable) or len(tuple(obj)) != 2:
+            raise TypeError
+
+        return Vector2(*obj)
+
+    def __mul__(self, other: SomeNumericType) -> 'Vector2':
+        if not isinstance(other, Number):
+            return NotImplemented
+        return Vector2(self.x * other, self.y * other)
+
+    def __truediv__(self, other: SomeNumericType) -> 'Vector2':
+        if not isinstance(other, Number):
+            return NotImplemented
+        return Vector2(self.x / other, self.y / other)
+
+    def __floordiv__(self, other: SomeNumericType) -> 'Vector2':
+        if not isinstance(other, Number):
+            return NotImplemented
+        return Vector2(self.x // other, self.y // other)
+
+    def __iter__(self) -> Iterator[NumericType]:
+        return iter((self.x, self.y))
+
+    def __getitem__(self, i: int):
+        if i == 0:
+            return self.x
+        elif i == 1:
+            return self.y
+        else:
+            raise IndexError
+
+    def __repr__(self) -> str:
+        return '{class_name}(x={self.x}, y={self.y})' \
+               .format(class_name=type(self).__name__, self=self)
+
+
+Vector2Like = Union[Vector2[NumericType], Tuple[NumericType, NumericType]]
 
 
 class Rect2(Generic[NumericType]):
@@ -14,10 +92,10 @@ class Rect2(Generic[NumericType]):
     def __init__(self, *, x: NumericType, y: NumericType, w: NumericType, h: NumericType): ...
 
     @overload
-    def __init__(self, *, p0: Tuple[NumericType, NumericType], p1: Tuple[NumericType, NumericType]): ...
+    def __init__(self, *, p0: Vector2Like[NumericType], p1: Vector2Like[NumericType]): ...
 
     @overload
-    def __init__(self, *, pos: Tuple[NumericType, NumericType], size: Tuple[NumericType, NumericType]): ...
+    def __init__(self, *, pos: Vector2Like[NumericType], size: Vector2Like[NumericType]): ...
 
     def __init__(self, **kwargs: NumericType):
         if 'x' in kwargs:
@@ -76,18 +154,18 @@ class Rect2(Generic[NumericType]):
         return self.y1 - self.y0
 
     @property
-    def p0(self) -> Tuple[NumericType, NumericType]:
-        return self.x0, self.y0
+    def p0(self) -> Vector2[NumericType]:
+        return Vector2(self.x0, self.y0)
 
     @property
-    def p1(self) -> Tuple[NumericType, NumericType]:
-        return self.x1, self.y1
+    def p1(self) -> Vector2[NumericType]:
+        return Vector2(self.x1, self.y1)
 
     pos = p0
 
     @property
-    def size(self) -> Tuple[NumericType, NumericType]:
-        return self.w, self.h
+    def size(self) -> Vector2[NumericType]:
+        return Vector2(self.w, self.h)
 
     def as_type(self, cast: Callable[[NumericType], SomeNumericType]) -> 'Rect2[SomeNumericType]':
         return Rect2(x0=cast(self.x0), y0=cast(self.y0), x1=cast(self.x1), y1=cast(self.y1))
@@ -106,9 +184,9 @@ class Rect2(Generic[NumericType]):
         return (self.p0 == other.p0) and (self.p1 == other.p1)
 
     def contains_point(self, point: Vector2, include_boundary: bool = True):
-        if include_boundary and (self.x0 <= point[0] <= self.x1 and self.y0 <= point[1] <= self.y1):
+        if include_boundary and (self.x0 <= point.x <= self.x1 and self.y0 <= point.y <= self.y1):
             return True
-        elif self.x0 < point[0] < self.x1 and self.y0 < point[1] < self.y1:
+        elif self.x0 < point.x < self.x1 and self.y0 < point.y < self.y1:
             return True
 
         return False
