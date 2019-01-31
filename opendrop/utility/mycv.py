57bdc99505b62a6f4105767e2b55b97f621f005d
eugenevorld <eugene_xhuang@hotmail.com>
2019-02-01 00:05:00 +0800

    Improve squish_contour()

--- opendrop/utility/mycv.py
+++ opendrop/utility/mycv.py
@@ -24,9 +24,9 @@ def find_contours(image):
     if CV2_VERSION >= (3, 2, 0):
         # In OpenCV 3.2, cv2.findContours() does not modify the passed image and instead returns the
         # modified image as the first, of the three, return values.
-        _, contours, hierarchy = cv2.findContours(image, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+        _, contours, hierarchy = cv2.findContours(image, cv2.RETR_LIST, cv2.CHAIN_APPROX_TC89_KCOS)
     else:
-        contours, hierarchy = cv2.findContours(image, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+        contours, hierarchy = cv2.findContours(image, cv2.RETR_LIST, cv2.CHAIN_APPROX_TC89_KCOS)
 
     # Each contour has shape (n, 1, 2) where 'n' is the number of points. Presumably this is so each
     # point is a size 2 column vector, we don't want this so reshape it to a (n, 2)
@@ -38,29 +38,54 @@ def find_contours(image):
     return contours
 
 
+# todo: add documentation
+def squish_contour(contour: np.ndarray) -> np.ndarray:
+    contour = contour.copy()
+
+    path_splice = 0
+    path = np.arange(len(contour))
+    objective = _polyline_l1(contour, idx=range(len(contour)))
+
+    for i in range(-1, -len(contour), -1):
+        path_splice_i = path_splice
+        path_i = path
+        objective_i = objective
+        decreasing = False
+
+        for j in range(path_splice, len(contour)):
+            path_splice_ij = j
+            path_ij = np.concatenate((path[:path_splice_ij], [i], path[path_splice_ij:-1]))
+            objective_ij = _polyline_l1(contour, path_ij)
+
+            if objective_ij < objective_i:
+                decreasing = True
+                path_i = path_ij
+                path_splice_i = j
+                objective_i = objective_ij
+            elif objective_ij > objective_i and decreasing:
+                break
+
+        if objective_i < objective:
+            path = path_i
+            path_splice = path_splice_i
+            objective = objective_i
+        elif objective_i > objective:
+            pass
+            break
+
+    squished = contour[path]
+    squished = _realign_squished_contour(squished)
+
+    return squished
+
+
+def _polyline_l1(polyline: np.ndarray, idx) -> float:
+    diff = np.diff(polyline[idx], axis=0)
+    length = np.sum(abs(diff))
+    return length
+
+
 def _realign_squished_contour(curve: np.ndarray) -> np.ndarray:
     dists = np.sum(abs(curve - np.roll(curve, shift=1, axis=0)), axis=1)
     start_idx = dists.argmax()
     return np.roll(curve, shift=-start_idx, axis=0)
-
-
-# todo: add documentation
-def squish_contour(contour: np.ndarray, radius: int = 2) -> np.ndarray:
-    contour = contour.copy()
-
-    squished = np.ndarray((0, 2), dtype=int)
-
-    i = [0]
-    v = contour[i[0]]
-    while len(contour):
-        dist = np.sum(abs(contour - v), axis=1)
-
-        i = np.where(dist <= dist.min() + radius)
-        v = np.average(contour[i], axis=0).astype(int)
-
-        squished = np.vstack((squished, v))
-        contour = np.delete(contour, i, axis=0)
-
-    squished = _realign_squished_contour(squished)
-
-    return squished
