6e8a7b27d8ee4e7cda11948e797e30ddf9642ab3
eugenevorld <eugene_xhuang@hotmail.com>
2019-02-25 23:02:51 +0800

    Add keyboard controls to LineDragToDefine tool

--- opendrop/app/common/content/image_processing/stage/tools.py
+++ opendrop/app/common/content/image_processing/stage/tools.py
@@ -1,5 +1,7 @@
-from typing import Optional
+import math
+from typing import Optional, Tuple
 
+from opendrop.utility import keyboard
 from opendrop.utility.bindable import AtomicBindable, AtomicBindableVar
 from opendrop.utility.geometry import Vector2, Rect2, Line2
 from opendrop.utility.misc import clamp
@@ -79,6 +81,70 @@ class LineDragToDefine(StageTool):
         if self._drag_start is None:
             return
         self._define_adjust(pos)
+
+    def do_keyboard_key_press(self, event: keyboard.KeyEvent) -> None:
+        if self.bn_selection is None:
+            return
+
+        if self._drag_start is not None:
+            # User is currently using mouse to define
+            return
+
+        if event.key is keyboard.Key.Up:
+            self._nudge_selection_up()
+        elif event.key is keyboard.Key.Down:
+            self._nudge_selection_down()
+        elif event.key is keyboard.Key.Left:
+            self._nudgerot_selection_anticlockwise()
+        elif event.key is keyboard.Key.Right:
+            self._nudgerot_selection_clockwise()
+        else:
+            return
+
+    def _nudge_selection_up(self) -> None:
+        # Smaller image y-coordinate is upwards
+        self._nudge_selection((0, -1))
+
+    def _nudge_selection_down(self) -> None:
+        self._nudge_selection((0, 1))
+
+    def _nudge_selection(self, delta: Tuple[float, float]) -> None:
+        line = self.bn_selection.get()
+        if line is None:
+            return
+
+        new_line = Line2(p0=line.p0 + delta,
+                         p1=line.p1 + delta)
+
+        self.bn_selection.set(new_line)
+
+    def _nudgerot_selection_clockwise(self) -> None:
+        self._nudgerot_selection(-0.001)
+
+    def _nudgerot_selection_anticlockwise(self) -> None:
+        self._nudgerot_selection(0.001)
+
+    def _nudgerot_selection(self, delta: float) -> None:
+        """Rotate the currently selected line anticlockwise by `delta` radians."""
+        line = self.bn_selection.get()
+        if line is None:
+            return
+
+        canvas_size = self._canvas_size
+        if canvas_size is not None:
+            center_x = canvas_size.x/2
+        else:
+            center_x = line.p0.x
+
+        line_angle = math.atan(line.gradient)
+        new_line_angle = line_angle - delta
+
+        new_p0 = line.eval_at(x=center_x)
+        new_p1 = new_p0 + (math.cos(new_line_angle), math.sin(new_line_angle))
+
+        new_line = Line2(p0=new_p0, p1=new_p1)
+
+        self.bn_selection.set(new_line)
 
     def _define_start(self, pos: Vector2[float]) -> None:
         self._drag_start = pos
