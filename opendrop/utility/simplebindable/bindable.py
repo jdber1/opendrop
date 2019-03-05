78167ba7a13a49275af4a2286b31ed1cfed9a72f
eugenevorld <eugene_xhuang@hotmail.com>
2019-03-05 21:49:36 +1100

    Add _general_purpose_equality_check()

--- opendrop/utility/simplebindable/bindable.py
+++ opendrop/utility/simplebindable/bindable.py
@@ -1,14 +1,28 @@
-import operator
+import math
 from abc import abstractmethod
+from typing import Any
 from typing import Generic, TypeVar, Callable, Optional
+
+import numpy as np
 
 from opendrop.utility.events import Event
 
 _T = TypeVar('_T')
 
 
+def _general_purpose_equality_check(x: Any, y: Any) -> bool:
+    if isinstance(x, float) and isinstance(y, float):
+        return math.isclose(x, y)
+
+    # Use np.array_equal() in case x or y are `np.ndarray`'s, in which case `bool(x == y)` will throw:
+    # > ValueError: The truth value of an array with more than one element is ambiguous...
+    # Even if x and y aren't both `np.ndarray`'s, the implementation of np.array_equal() means that the result will
+    # be identical to evaluating `x == y`, although this behaviour does not seem to be guaranteed by documentation.
+    return np.array_equal(x, y)
+
+
 class Bindable(Generic[_T]):
-    def __init__(self, check_equals: Callable[[_T, _T], bool] = operator.eq) -> None:
+    def __init__(self, check_equals: Callable[[_T, _T], bool] = _general_purpose_equality_check) -> None:
         self.on_changed = Event()
 
         self._check_equals = check_equals
