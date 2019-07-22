0eeafc04d88256f07c16d4665a2705ed0efec2a5
eugenevorld <eugene_xhuang@hotmail.com>
2019-07-22 21:22:45 +0800

    Add MaskFill RenderObject

--- opendrop/widgets/render/objects/__init__.py
+++ opendrop/widgets/render/objects/__init__.py
@@ -1,5 +1,6 @@
 from .angle import Angle
 from .line import Line
+from .mask_fill import MaskFill
 from .pixbuf_fill import PixbufFill
 from .polyline import Polyline
 from .rectangle import Rectangle, RectangleWithLabel
---
+++ opendrop/widgets/render/objects/mask_fill.py
@@ -0,0 +1,54 @@
+from typing import Tuple, Optional
+
+import cairo
+import numpy as np
+from gi.repository import GObject
+
+from opendrop.utility.cairomisc import cairo_saved
+from .. import abc
+
+
+class MaskFill(abc.RenderObject):
+    def _do_draw(self, cr: cairo.Context) -> None:
+        mask = self._mask
+        if mask is None:
+            return
+
+        scale = self._parent._widget_dist_from_canvas((1, 1))
+        offset = self._parent._widget_coord_from_canvas((0, 0))
+
+        with cairo_saved(cr):
+            cr.translate(*offset)
+            cr.scale(*scale)
+
+            mask_surface = cairo.ImageSurface.create_for_data(
+                memoryview(mask),
+                cairo.FORMAT_A8,
+                mask.shape[1],
+                mask.shape[0],
+            )
+
+            cr.set_source_rgb(*self._color)
+            cr.mask_surface(mask_surface, 0, 0)
+
+    _mask = None  # type: Optional[np.ndarray]
+
+    @GObject.Property
+    def mask(self) -> np.ndarray:
+        return self._mask
+
+    @mask.setter
+    def mask(self, mask: np.ndarray) -> None:
+        self._mask = mask
+        self.emit('request-draw')
+
+    _color = (0.0, 0.0, 0.0)
+
+    @GObject.Property
+    def color(self) -> Tuple[float, float, float]:
+        return self._color
+
+    @color.setter
+    def color(self, color: Tuple[float, float, float]) -> None:
+        self._color = color
+        self.emit('request-draw')
