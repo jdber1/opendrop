a6080f1dc20b6b998e86815c902524c86c3052bd
eugenevorld <eugene_xhuang@hotmail.com>
2019-07-31 12:54:21 +1000

    Add placeholder for 'author' and 'copyright'

--- docs/conf.py
+++ docs/conf.py
@@ -18,8 +18,8 @@ sys.path.insert(0, os.path.abspath('../'))
 # -- Project information -----------------------------------------------------
 
 project = 'OpenDrop'
-# copyright = ''
-# author = ''
+copyright = 'OpenDrop Contributors'
+author = 'OpenDrop Contributors'
 
 
 # -- General configuration ---------------------------------------------------
