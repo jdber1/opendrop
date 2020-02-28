f781fd05ee6b406d57dd449b93e40987b5bdc5c3
eugenevorld <eugene_xhuang@hotmail.com>
2020-02-27 21:21:36 -0800

    Fix license formatting

--- opendrop/app/common/analysis_saver/figure_options/model.py
+++ opendrop/app/common/analysis_saver/figure_options/model.py
@@ -11,20 +11,22 @@
 # pendant drop tensiometry. Journal of Colloid and Interface Science 454
 # (2015) 226–237. https://doi.org/10.1016/j.jcis.2015.05.012
 #
-#E. Huang, T. Denning, A. Skoufis, J. Qi, R. R. Dagastine, R. F. Tabor
-#and J. D. Berry, OpenDrop: Open-source software for pendant drop
-#tensiometry & contact angle measurements, submitted to the Journal of
+# E. Huang, T. Denning, A. Skoufis, J. Qi, R. R. Dagastine, R. F. Tabor
+# and J. D. Berry, OpenDrop: Open-source software for pendant drop
+# tensiometry & contact angle measurements, submitted to the Journal of
 # Open Source Software
 #
-#These citations help us not only to understand who is using and
-#developing OpenDrop, and for what purpose, but also to justify
-#continued development of this code and other open source resources.
+# These citations help us not only to understand who is using and
+# developing OpenDrop, and for what purpose, but also to justify
+# continued development of this code and other open source resources.
 #
 # OpenDrop is distributed WITHOUT ANY WARRANTY; without even the
 # implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 # PURPOSE.  See the GNU General Public License for more details.  You
 # should have received a copy of the GNU General Public License along
 # with this software.  If not, see <https://www.gnu.org/licenses/>.
+
+
 from typing import Tuple
 
 from opendrop.utility.bindable import VariableBindable
