2c5d2506862617bccd2cb2f348928eede884be76
Eugene Huang <eugene.huang99@gmail.com>
2020-07-03 17:08:29 +1000

    Add PEP 517 build system
    With the intention of automating GLib resource compilation.

---
+++ build_backend/buildapi.py
@@ -0,0 +1,6 @@
+def build_wheel(wheel_directory, config_settings=None, metadata_directory=None):
+    raise NotImplementedError
+
+
+def build_sdist(sdist_directory, config_settings=None):
+    raise NotImplementedError
