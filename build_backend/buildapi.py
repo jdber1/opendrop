7942562da2fd94c8b3470681f99d4575ea9c470f
Eugene Huang <eugene.huang99@gmail.com>
2020-07-09 17:19:43 +1000

    Remove custom build backend, it's a waste of time

--- build_backend/buildapi.py
+++
@@ -1,6 +0,0 @@
-def build_wheel(wheel_directory, config_settings=None, metadata_directory=None):
-    raise NotImplementedError
-
-
-def build_sdist(sdist_directory, config_settings=None):
-    raise NotImplementedError
