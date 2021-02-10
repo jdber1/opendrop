402e54fb53abe0130cb80ba2e56457d176dd0148
Eugene Huang <eugene.huang99@gmail.com>
2021-02-10 11:50:52 +0800

    Reimplement _general_purpose_equality_check
    Currently defaults to using 'is' to check for equality when x and y are
    not numbers/arrays. This does not work for non-interned strings and
    other types without unique instances. This can cause an infinite loop in
    Bindings.

--- opendrop/utility/bindable/misc.py
+++ opendrop/utility/bindable/misc.py
@@ -7,7 +7,7 @@ import numpy as np
 def _general_purpose_equality_check(x: Any, y: Any) -> bool:
     if isinstance(x, numbers.Number) and isinstance(y, numbers.Number):
         return np.allclose(x, y, atol=0, equal_nan=True)
-    elif isinstance(x, np.ndarray) and isinstance(y, np.ndarray):
+    elif isinstance(x, np.ndarray) or isinstance(y, np.ndarray):
         return np.array_equal(x, y)
     else:
-        return x is y
+        return x == y
