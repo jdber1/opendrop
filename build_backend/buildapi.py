2481ee7d5510dc8ac8c053f1e04b95425719f5ff
Eugene Huang <eugene.huang99@gmail.com>
2022-08-01 21:42:13 +0800

    Manually specify PYTHON when using python PEP 517 buildapi

--- build_backend/buildapi.py
+++ build_backend/buildapi.py
@@ -1,9 +1,15 @@
+import os
 from pathlib import Path
 import subprocess
+import sys
 
 
 def build_wheel(wheel_directory, config_settings=None, metadata_directory=None):
-    subprocess.run(['scons', '-Q', 'bdist_wheel', '--build-dir=' + wheel_directory], check=True)
+    env = os.environ.copy()
+    if sys.executable is not None:
+        env['PYTHON'] = sys.executable
+
+    subprocess.run(['scons', '-Q', 'bdist_wheel', '--build-dir=' + wheel_directory], check=True, env=env)
 
     try:
         fp, = Path(wheel_directory).glob('*.whl')
