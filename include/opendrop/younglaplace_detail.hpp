jdber1/opendrop

commit f720ea55943bd7d3cf0949ee331bf0b05fb8f9ea

Add check to YoungLaplaceShape for zero sized steps
When bond is ~ 0, step_DBo() could advance by zero near s = pi, the
integrator warns us with ARK_WARNING (indicating the next h is so small
such that t + h == t) but we didn't handle this since it's technically
not an error.

include/opendrop/younglaplace_detail.hpp
@@ -442,20 +442,22 @@ void
 YoungLaplaceShape<realtype>::step_DBo()
 {
     int flag;
-    detail::sunreal tcur, tnext;
+    detail::sunreal told, tcur, tnext;
 
-    flag = ERKStepGetCurrentTime(arkode_mem_DBo, &tcur);
+    flag = ERKStepGetCurrentTime(arkode_mem_DBo, &told);
     if (flag == ARK_MEM_NULL) throw std::runtime_error("ARK_MEM_NULL");
 
     if (tcur == RCONST(0.0)) {
         // First step. Set tout to 0.1 to give a rough scale of t variable when using ARK_ONE_STEP.
-        tnext = tcur + RCONST(0.1);
+        tnext = told + RCONST(0.1);
     } else {
         tnext = INFINITY;
     }
 
     flag = ERKStepEvolve(arkode_mem_DBo, tnext, nv_DBo, &tcur, ARK_ONE_STEP);
     if (flag < 0) throw std::runtime_error("ERKStepEvolve() failed.");
+
+    if (tcur == told) throw std::runtime_error("ERKStepEvolve() failed: step size too small.");
 
     realtype y[2], dy_ds[2], d2y_ds2[2];
     y[0] = NV_Ith_S(nv_DBo, 0);

commit 2f7dc0eda952eb4a1024fba480f1c9f547cc5829

Fix bug with YoungLaplaceShape::step_DBo() failing
Bug introduced in https://github.com/jdber1/opendrop/commit/f720ea55943bd7d3cf0949ee331bf0b05fb8f9ea

Surprising it wasn't caught earlier. "Current" time at start of function
is stored in told, tcur is uninitialized. If told is 0.0, then it is the
first step and we should give a sensible target time (tnext) for
ERKStepEvolve(), as per docs, instead of INFINITY.

Why this used to work, I do not know, and in fact it still sometimes
works now. (Note for completeness: my C++ test code works, but not when
accessing through Cython interface, and in both cases the tcur != 0.0
and tnext = INFINITY).

Link: https://github.com/jdber1/opendrop/issues/46

include/opendrop/younglaplace_detail.hpp
@@ -469,7 +469,7 @@ YoungLaplaceShape<realtype>::step_DBo()
     flag = ERKStepGetCurrentTime(arkode_mem_DBo, &told);
     if (flag == ARK_MEM_NULL) throw std::runtime_error("ARK_MEM_NULL");
 
-    if (tcur == RCONST(0.0)) {
+    if (told == RCONST(0.0)) {
         // First step. Set tout to 0.1 to give a rough scale of t variable when using ARK_ONE_STEP.
         tnext = told + RCONST(0.1);
     } else {