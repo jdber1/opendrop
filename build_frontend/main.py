f0bd64f87d86bf7e138668ce7a9d72a43d7e679b
ToriLi1210 <hanying1@student.unimelb.edu.au>
2024-10-10 00:06:24 +1100

    feat(main.py): create frontend templates

---
+++ build_frontend/main.py
@@ -0,0 +1,12 @@
+from customtkinter import CTk
+from ui.main_window import MainWindow  # Import the MainWindow class from main_window.py
+
+def main():
+    # Create the main application window
+    app = MainWindow()
+    # Run the application
+    app.mainloop()
+
+if __name__ == "__main__":
+    main()
+
