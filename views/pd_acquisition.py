e5677a05437420eb2e941c6e6b960f5e11381ca5
Rin <lvqq2000@gmail.com>
2024-10-11 18:43:42 +1100

    refactor(pd_acquisition.py): modular the code

--- views/pd_acquisition.py
+++ views/pd_acquisition.py
@@ -12,34 +12,54 @@ class PdAcquisition(CTkFrame):
         super().__init__(parent, **kwargs)
 
         self.image_handler = ImageHandler()
+        self.user_input_data = user_input_data
+        self.current_index = 0
 
+        self.frame_interval_var = StringVar()
+        self.frame_interval_var.trace_add("write", self.update_frame_interval)
+
+        self.setup_image_source_frame()
+        self.setup_choose_files_frame()
+        self.setup_frame_interval_frame()
+
+        self.images_frame = CTkFrame(self)
+
+    def setup_image_source_frame(self):
+        """Set up the image source frame and its components."""
         self.image_source_frame = CTkFrame(self, fg_color="transparent")
         self.image_source_frame.pack(pady=20)
         self.create_component_label(self.image_source_frame, "Image Source: ")
         self.image_source_button = CTkButton(self.image_source_frame, text="Filesystem", width=150, height=30)
         self.image_source_button.pack(side="left")
 
+    def setup_choose_files_frame(self):
+        """Set up the choose files frame and its components."""
         self.choose_files_frame = CTkFrame(self, fg_color="transparent")
         self.choose_files_frame.pack(pady=10)
         self.create_component_label(self.choose_files_frame, "Image Files: ")
-        self.choose_files_button = CTkButton(self.choose_files_frame, text="Choose File(s)", command=lambda: self.select_files(), width=150, height=30)
+        self.choose_files_button = CTkButton(
+            self.choose_files_frame,
+            text="Choose File(s)",
+            command=self.select_files,
+            width=150,
+            height=30
+        )
         self.choose_files_button.pack(side="left")
 
-        self.frame_interval_var = StringVar()
-        self.frame_interval_var.trace_add("write", self.update_frame_interval)
-
-        numberic_validate_command = self.register(validate_numeric_input)
-
+    def setup_frame_interval_frame(self):
+        """Set up the frame interval input."""
         self.frame_interval_frame = CTkFrame(self, fg_color="transparent")
         self.create_component_label(self.frame_interval_frame, "Frame Intervals:")
-        self.frame_interval_entry = CTkEntry(self.frame_interval_frame, textvariable=self.frame_interval_var, validate="key", validatecommand=(numberic_validate_command, '%P'), width=150, height=30)
+        numberic_validate_command = self.register(validate_numeric_input)
+        self.frame_interval_entry = CTkEntry(
+            self.frame_interval_frame,
+            textvariable=self.frame_interval_var,
+            validate="key",
+            validatecommand=(numberic_validate_command, '%P'),
+            width=150,
+            height=30
+        )
         self.frame_interval_entry.pack(side="left")
-
-        self.images_frame = CTkFrame(self)
-
-        self.current_index = 0
-
-        self.user_input_data = user_input_data
 
     def select_files(self):
         # Clear previous images
@@ -68,43 +88,40 @@ class PdAcquisition(CTkFrame):
 
             self.images_frame.pack(pady=20)
 
-            # Image display area
-            self.image_label = CTkLabel(self.images_frame, text="", fg_color="lightgrey", width=400, height=300)
-            self.image_label.pack()
-
-            # Display the filename below the image
-            file_name = os.path.basename(self.user_input_data.import_files[self.current_index])
-            self.name_label = CTkLabel(self.images_frame, text=file_name, font=("Arial", 10))
-            self.name_label.pack()
-
-            # Frame to hold navigation components
-            self.image_navigation_frame = CTkFrame(self.images_frame)
-            self.image_navigation_frame.pack(pady=20)
-
-            # Previous button
-            self.prev_button = CTkButton(self.image_navigation_frame, text="<", command=lambda: self.change_image(-1), width=3)
-            self.prev_button.pack(side="left", padx=10)
-
-            # Initialize entry for current index
-            self.index_entry = CTkEntry(self.image_navigation_frame, width=5)
-            self.index_entry.pack(side="left")
-            self.index_entry.bind("<Return>", lambda event: self.update_index_from_entry())
-            self.index_entry.insert(0, str(self.current_index + 1))
-
-            # Navigation label
-            self.navigation_label = CTkLabel(self.image_navigation_frame, text=f" of {num_files}", font=("Arial", 12))
-            self.navigation_label.pack(side="left")
-
-            # Next button
-            self.next_button = CTkButton(self.image_navigation_frame, text=">", command=lambda: self.change_image(1), width=3)
-            self.next_button.pack(side="left", padx=10)
-
-            self.load_image(self.user_input_data.import_files[self.current_index])  # Load the first image by default
+            self.initialize_image_display(self)
         else:
             self.frame_interval_frame.pack_forget()
             self.images_frame.pack_forget()
             self.choose_files_button.configure(text="Choose File(s)")  # Reset if no files were chosen
             messagebox.showinfo("No Selection", "No files were selected.")
+
+    def initialize_image_display(self):
+        """Initialize the image display area."""
+        self.image_label = CTkLabel(self.images_frame, text="", fg_color="lightgrey", width=400, height=300)
+        self.image_label.pack()
+
+        file_name = os.path.basename(self.user_input_data.import_files[self.current_index])
+        self.name_label = CTkLabel(self.images_frame, text=file_name, font=("Arial", 10))
+        self.name_label.pack()
+
+        self.image_navigation_frame = CTkFrame(self.images_frame)
+        self.image_navigation_frame.pack(pady=20)
+
+        self.prev_button = CTkButton(self.image_navigation_frame, text="<", command=lambda: self.change_image(-1), width=3)
+        self.prev_button.pack(side="left", padx=10)
+
+        self.index_entry = CTkEntry(self.image_navigation_frame, width=5)
+        self.index_entry.pack(side="left")
+        self.index_entry.bind("<Return>", lambda event: self.update_index_from_entry())
+        self.index_entry.insert(0, str(self.current_index + 1))
+
+        self.navigation_label = CTkLabel(self.image_navigation_frame, text=f" of {self.user_input_data.number_of_frames}", font=("Arial", 12))
+        self.navigation_label.pack(side="left")
+
+        self.next_button = CTkButton(self.image_navigation_frame, text=">", command=lambda: self.change_image(1), width=3)
+        self.next_button.pack(side="left", padx=10)
+
+        self.load_image(self.user_input_data.import_files[self.current_index])
 
     def load_image(self, selected_image):
         """Load and display the selected image."""
