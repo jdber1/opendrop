c86cbee0d563b88cb6c98f0251bc01266dc2f591
Rin <lvqq2000@gmail.com>
2024-10-16 19:29:44 +1100

    feat(ui): change all the tk widget to ctk widget
    Ensure the ui design of both functionalities are aligned with each other

--- views/component/check_button.py
+++ views/component/check_button.py
@@ -1,20 +1,22 @@
 import tkinter as tk
+import customtkinter as ctk
 
 from utils.config import *
 
 class CheckButton():
-    def __init__(self, parent, frame, text_left, callback, rw=0, cl=0, width_specify=10, pdx=0, pdy=2, stcky="w", state_specify='normal'):  # , pd=5
+    def __init__(self, parent, frame, text_left, callback, rw=0, cl=0, width_specify=10, padx=(5, 5), pady=(1, 1), stcky="w", state_specify='normal'):  # , pd=5
         self._save_previous_variable = 0
-        self.int_variable = tk.IntVar()
+        self.int_variable = ctk.IntVar()
 
         if callback:
             self.int_variable.trace_add("write", callback)
 
-        self.check_button = tk.Checkbutton(
-            frame, text=text_left, background=BACKGROUND_COLOR, variable=self.int_variable, state=state_specify)
+        self.check_button = ctk.CTkCheckBox(
+            frame, text=text_left, variable=self.int_variable, state=state_specify, 
+            border_width=2, checkbox_width=15, checkbox_height=15, corner_radius=5)
         # "CENTER") # sticky="w" padx=pd,
         self.check_button.grid(
-            row=rw, column=cl, sticky=stcky, pady=pdy, padx=pdx)
+            row=rw, column=cl, sticky=stcky, pady=pady, padx=padx)
 
     def get_value(self):
         return self.int_variable.get()
@@ -25,14 +27,14 @@ class CheckButton():
     def disable(self):
         self._save_previous_variable = self.get_value()
         self.set_value(0)
-        self.check_button.config(state="disabled")
+        self.check_button.configure(state="disabled")
 
     def normal(self):
         self.set_value(self._save_previous_variable)
-        self.check_button.config(state="normal")
+        self.check_button.configure(state="normal")
 
     def state(self):
-        return self.check_button.config()['state'][-1]
+        return self.check_button.configure()['state'][-1]
 
     def grid_forget(self):
         self.check_button.grid_forget()
--- views/component/float_entry.py
+++ views/component/float_entry.py
@@ -1,24 +1,24 @@
 import tkinter as tk
+import customtkinter as ctk
 
 from utils.config import *
 from utils.validators import *
 
 class FloatEntry():
-    def __init__(self, parent, frame, text_left, callback, rw=0, label_width=None, width_specify=10, state_specify='normal'):
-        self.label = tk.Label(frame, text=text_left,
-                              background=BACKGROUND_COLOR, width=label_width)
-        self.label.grid(row=rw, column=0, sticky="w")
-        self.text_variable = tk.StringVar()
+    def __init__(self, parent, frame, text_left, callback, rw=0, padx=(5, 5), pady=(5, 5), width_specify=150, label_width=150, state_specify='normal'):
+        self.label = ctk.CTkLabel(frame, text=text_left, width=label_width)
+        self.label.grid(row=rw, column=0, sticky="w", padx=padx, pady=pady)
+        self.text_variable = ctk.StringVar()
 
         if callback:
             self.text_variable.trace_add("write", callback)
             
         vcmd_float = (parent.register(validate_float),
                       '%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W')
-        self.entry = tk.Entry(frame, highlightbackground=BACKGROUND_COLOR,
+        self.entry = ctk.CTkEntry(frame,
                               textvariable=self.text_variable, validate='key', validatecommand=vcmd_float)
-        self.entry.config(width=width_specify, state=state_specify)
-        self.entry.grid(row=rw, column=1, sticky="we")
+        self.entry.configure(width=width_specify, state=state_specify)
+        self.entry.grid(row=rw, column=1, sticky="we", padx=padx, pady=pady)
 
     def get_value(self):
         return float("0" + self.text_variable.get())
@@ -27,9 +27,9 @@ class FloatEntry():
         self.text_variable.set(str(float(value)))
 
     def disable(self):
-        self.entry.config(state="disabled")
-        self.label.config(state="disabled")
+        self.entry.configure(state="disabled")
+        self.label.configure(state="disabled")
 
     def normal(self):
-        self.entry.config(state="normal")
-        self.label.config(state="normal")
+        self.entry.configure(state="normal")
+        self.label.configure(state="normal")
